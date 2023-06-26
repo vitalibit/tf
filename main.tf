25f61389ed20c4fdebca69926b16258a16b4b20c
vitalibit <vitaliy081098@gmail.com>
2023-06-26 22:07:17 +0000

    GKE_NUM_NODES modified to var

--- main.tf
+++ main.tf
@@ -2,5 +2,12 @@ module "gke_cluster" {
   source         = "github.com/vitalibit/tf-google-gke-cluster"
   GOOGLE_REGION  = var.GOOGLE_REGION
   GOOGLE_PROJECT = var.GOOGLE_PROJECT
-  GKE_NUM_NODES  = 2
+  GKE_NUM_NODES  = var.GKE_NUM_NODES
+}
+
+terraform {
+  backend "gcs" {
+    bucket = "k8s-k3s-tf-state"
+    prefix = "terraform/state"
+  }
 }

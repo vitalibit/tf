c07a239b22c77961a3df6a008f25d85d7299c7fe
vitalibit <vitaliy081098@gmail.com>
2023-06-29 00:59:04 +0000

    changed bootstrap and kind modules

--- main.tf
+++ main.tf
@@ -15,14 +15,17 @@ module "github_repository" {
 # }
 
 module "kind_cluster" {
-  source = "github.com/den-vasyliev/tf-kind-cluster"
+  source = "github.com/den-vasyliev/tf-kind-cluster?ref=cert_auth"
 }
 
 module "flux_bootstrap" {
-  source            = "github.com/den-vasyliev/tf-fluxcd-flux-bootstrap"
+  source            = "github.com/den-vasyliev/tf-fluxcd-flux-bootstrap?ref=kind_auth"
   github_repository = "${var.GITHUB_OWNER}/${var.FLUX_GITHUB_REPO}"
   private_key       = module.tls_private_key.private_key_pem
-  config_path       = module.kind_cluster.kubeconfig
+  config_host       = module.kind_cluster.endpoint
+  config_client_key = module.kind_cluster.client_key
+  config_ca         = module.kind_cluster.ca
+  config_crt        = module.kind_cluster.crt
   github_token      = var.GITHUB_TOKEN
 }
 

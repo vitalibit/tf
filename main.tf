adbd88e4515d7fd531c0bf4d72eee59e9da3947a
vitalibit <vitaliy081098@gmail.com>
2023-07-09 21:39:50 +0000

    update to using gke cluster

--- main.tf
+++ main.tf
@@ -7,25 +7,19 @@ module "github_repository" {
   public_key_openssh_title = "flux0"
 }
 
-# module "gke_cluster" {
-# source         = "github.com/vitalibit/tf-google-gke-cluster"
-# GOOGLE_REGION  = var.GOOGLE_REGION
-# GOOGLE_PROJECT = var.GOOGLE_PROJECT
-# GKE_NUM_NODES  = var.GKE_NUM_NODES
-# }
-
-module "kind_cluster" {
-  source = "github.com/den-vasyliev/tf-kind-cluster?ref=cert_auth"
+module "gke_cluster" {
+source         = "github.com/vitalibit/tf-google-gke-cluster"
+GOOGLE_REGION  = var.GOOGLE_REGION
+GOOGLE_PROJECT = var.GOOGLE_PROJECT
+GKE_NUM_NODES  = var.GKE_NUM_NODES
 }
 
+
 module "flux_bootstrap" {
-  source            = "github.com/den-vasyliev/tf-fluxcd-flux-bootstrap?ref=kind_auth"
+  source            = "./modules/fluxcd-flux-bootstrap"
   github_repository = "${var.GITHUB_OWNER}/${var.FLUX_GITHUB_REPO}"
   private_key       = module.tls_private_key.private_key_pem
-  config_host       = module.kind_cluster.endpoint
-  config_client_key = module.kind_cluster.client_key
-  config_ca         = module.kind_cluster.ca
-  config_crt        = module.kind_cluster.crt
+  config_path       = module.gke_cluster.kubeconfig
   github_token      = var.GITHUB_TOKEN
 }
 
@@ -33,3 +27,24 @@ module "tls_private_key" {
   source    = "github.com/den-vasyliev/tf-hashicorp-tls-keys"
   algorithm = "RSA"
 }
+
+# module "gke-workload-identity" {
+#   source              = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
+#   use_existing_k8s_sa = true
+#   name                = "kustomize-controller"
+#   namespace           = "flux-system"
+#   project_id          = var.GOOGLE_PROJECT
+#   cluster_name        = "main"
+#   location            = var.GOOGLE_REGION
+#   annotate_k8s_sa     = true
+#   roles               = ["roles/cloudkms.cryptoKeyEncrypterDecrypter"]
+# }
+
+# module "kms" {
+#   source          = "github.com/den-vasyliev/terraform-google-kms"
+#   project_id      = var.GOOGLE_PROJECT
+#   keyring         = "sops-flux"
+#   location        = "global"
+#   keys            = ["sops-key-flux"]
+#   prevent_destroy = false
+# }

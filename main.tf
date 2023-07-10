8a22bf38677012f50f3e1d799d7490c991cd1707
vitalibit <vitaliy081098@gmail.com>
2023-07-10 00:29:15 +0000

    update fmt

--- main.tf
+++ main.tf
@@ -8,15 +8,15 @@ module "github_repository" {
 }
 
 module "gke_cluster" {
-source         = "github.com/vitalibit/tf-google-gke-cluster"
-GOOGLE_REGION  = var.GOOGLE_REGION
-GOOGLE_PROJECT = var.GOOGLE_PROJECT
-GKE_NUM_NODES  = var.GKE_NUM_NODES
+  source         = "github.com/vitalibit/tf-google-gke-cluster"
+  GOOGLE_REGION  = var.GOOGLE_REGION
+  GOOGLE_PROJECT = var.GOOGLE_PROJECT
+  GKE_NUM_NODES  = var.GKE_NUM_NODES
 }
 
 
 module "flux_bootstrap" {
-  source            = "./modules/fluxcd-flux-bootstrap"
+  source            = "github.com/den-vasyliev/tf-fluxcd-flux-bootstrap"
   github_repository = "${var.GITHUB_OWNER}/${var.FLUX_GITHUB_REPO}"
   private_key       = module.tls_private_key.private_key_pem
   config_path       = module.gke_cluster.kubeconfig
@@ -28,23 +28,23 @@ module "tls_private_key" {
   algorithm = "RSA"
 }
 
-# module "gke-workload-identity" {
-#   source              = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
-#   use_existing_k8s_sa = true
-#   name                = "kustomize-controller"
-#   namespace           = "flux-system"
-#   project_id          = var.GOOGLE_PROJECT
-#   cluster_name        = "main"
-#   location            = var.GOOGLE_REGION
-#   annotate_k8s_sa     = true
-#   roles               = ["roles/cloudkms.cryptoKeyEncrypterDecrypter"]
-# }
+module "gke-workload-identity" {
+  source              = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
+  use_existing_k8s_sa = true
+  name                = "kustomize-controller"
+  namespace           = "flux-system"
+  project_id          = var.GOOGLE_PROJECT
+  cluster_name        = "main"
+  location            = var.GOOGLE_REGION
+  annotate_k8s_sa     = true
+  roles               = ["roles/cloudkms.cryptoKeyEncrypterDecrypter"]
+}
 
-# module "kms" {
-#   source          = "github.com/den-vasyliev/terraform-google-kms"
-#   project_id      = var.GOOGLE_PROJECT
-#   keyring         = "sops-flux"
-#   location        = "global"
-#   keys            = ["sops-key-flux"]
-#   prevent_destroy = false
-# }
+module "kms" {
+  source          = "github.com/den-vasyliev/terraform-google-kms"
+  project_id      = var.GOOGLE_PROJECT
+  keyring         = "sops-flux-v2"
+  location        = "global"
+  keys            = ["sops-key-flux-v2"]
+  prevent_destroy = false
+}
